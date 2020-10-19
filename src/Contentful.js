8aeb97ec9e6e9cdf5ce7ea7a7c4c44b88eb33f3e
YourGuruNet <arvels123@inbox.lv>
2020-10-19 19:42:05 +0300

    ..

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,6 +1,6 @@
 import { createClient } from "contentful";
 
 export default createClient({
-  space: "mab1ksy20us7",
-  accessToken: "6WSOTCEBFVRkmQ2ILMh58UJJOsfYu4ezmoVNVaH0Hxk",
+  space: process.env.REACT_APP_API_SPACE,
+  accessToken: process.env.REACT_APP_ACCESS_TOKEN,
 });
